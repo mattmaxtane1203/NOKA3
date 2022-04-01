f021303eecaf5151fc5dbedf396f211f59db5ec2
mattmaxtane1203 <mattmaxtane1203@gmail.com>
2022-04-01 14:31:59 +0700

    first commit

---
+++ script/redirect.js
@@ -0,0 +1,21 @@
+// Scripting for redirecting to web pages
+
+function home(){
+    window.open("../html/esports.html", "_self")
+}
+
+function heroes(){
+    window.open("../html/heroes.html", "_self")
+}
+
+function news(){
+    window.open("../html/news.html", "_self")
+}
+
+function esports(){
+    window.open("../html/esports.html", "_self")
+}
+
+function registerNow(){
+    window.open("../html/registerNow.html", "_self")
+}
